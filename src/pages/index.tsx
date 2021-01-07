a83c1a9814d2eebff5cc3d2ccba85f2911072b9b
Juliano Costa Silva <apfjuliano@gmail.com>
2021-01-07 16:13:50 -0300

    Add validation using Yup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,20 @@
 import { Card, CardContent } from '@material-ui/core';
 import { Field, Form, Formik } from 'formik';
 import { CheckboxWithLabel, TextField } from 'formik-material-ui';
+import { mixed, number, object } from 'yup';
 
 export default function Home() {
   return (
     <Card>
       <CardContent>
       <Formik
+      validationSchema={object({
+        money: mixed().when('millionaire', {
+          is: true,
+          then: number().required().min(1_000_000, "Money must be greater than or equal to 1.000.000"),
+          otherwise: number().required("Please fill out this field")
+        })
+      })}
           initialValues={{
             firstName: '',
             lastName: '',
@@ -15,7 +23,7 @@ export default function Home() {
             description: '',
           }}
           onSubmit={() => {}}>
-            <Form>
+            <Form autoComplete="off">
               <Field name="firstName" component={TextField} label="First Name" />
               <Field name="lastName" component={TextField} label="Last Name" />
               <Field name="millionaire" type="checkbox" component={CheckboxWithLabel} Label={{ label: 'I am a millionaire' }} />
