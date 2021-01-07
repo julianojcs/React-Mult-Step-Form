4594a21adb7ebdfd2f7c52b8376e1a9dd61f7856
Juliano Costa Silva <apfjuliano@gmail.com>
2021-01-07 15:56:27 -0300

    Form with fields, but without any steps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,28 @@
-import { Card, CardContent, Typography } from '@material-ui/core';
+import { Card, CardContent } from '@material-ui/core';
+import { Field, Form, Formik } from 'formik';
+import { CheckboxWithLabel, TextField } from 'formik-material-ui';
 
 export default function Home() {
   return (
     <Card>
       <CardContent>
-        <Typography variant="h1">Hello World!</Typography>
+      <Formik
+          initialValues={{
+            firstName: '',
+            lastName: '',
+            millionaire: false,
+            money: 0,
+            description: '',
+          }}
+          onSubmit={() => {}}>
+            <Form>
+              <Field name="firstName" component={TextField} label="First Name" />
+              <Field name="lastName" component={TextField} label="Last Name" />
+              <Field name="millionaire" type="checkbox" component={CheckboxWithLabel} Label={{ label: 'I am a millionaire' }} />
+              <Field name="money" type="number" component={TextField} label="All the money I have" />
+              <Field name="description" component={TextField} label="Description" />
+            </Form>
+          </Formik>
       </CardContent>
     </Card>
   );
