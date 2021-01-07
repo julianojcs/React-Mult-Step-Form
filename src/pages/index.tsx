48681e289b8afcc22f8b1d41527386729639c955
Juliano Costa Silva <apfjuliano@gmail.com>
2021-01-07 16:25:36 -0300

    Creating the Component FormikStepper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent } from '@material-ui/core';
-import { Field, Form, Formik } from 'formik';
+import { Field, Form, Formik, FormikConfig, FormikValues } from 'formik';
 import { CheckboxWithLabel, TextField } from 'formik-material-ui';
 import { mixed, number, object } from 'yup';
 
@@ -7,11 +7,11 @@ export default function Home() {
   return (
     <Card>
       <CardContent>
-      <Formik
+      <FormikStepper
       validationSchema={object({
         money: mixed().when('millionaire', {
           is: true,
-          then: number().required().min(1_000_000, "Money must be greater than or equal to 1.000.000"),
+          then: number().required().min(1_000_000, "Becouse you said you are a millionare you need to have 1 million"),
           otherwise: number().required("Please fill out this field")
         })
       })}
@@ -24,14 +24,28 @@ export default function Home() {
           }}
           onSubmit={() => {}}>
             <Form autoComplete="off">
-              <Field name="firstName" component={TextField} label="First Name" />
-              <Field name="lastName" component={TextField} label="Last Name" />
-              <Field name="millionaire" type="checkbox" component={CheckboxWithLabel} Label={{ label: 'I am a millionaire' }} />
-              <Field name="money" type="number" component={TextField} label="All the money I have" />
-              <Field name="description" component={TextField} label="Description" />
+              <div>
+                <Field name="firstName" component={TextField} label="First Name" />
+                <Field name="lastName" component={TextField} label="Last Name" />
+                <Field name="millionaire" type="checkbox" component={CheckboxWithLabel} Label={{ label: 'I am a millionaire' }} />
+              </div>
+              <div>
+                <Field name="money" type="number" component={TextField} label="All the money I have" />
+              </div>
+              <div>
+                <Field name="description" component={TextField} label="Description" />
+              </div>
             </Form>
-          </Formik>
+          </FormikStepper>
       </CardContent>
     </Card>
   );
+}
+
+export function FormikStepper({children, ...props}: FormikConfig<FormikValues>) {
+  return (
+    <Formik {...props} >
+      <Form autoComplete="off">{children}</Form>
+    </Formik>
+  )
 }
